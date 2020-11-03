6ee7c5488acd51dad90160917ec06d746e24e550
Chaha1n <chahaaaaan@gmail.com>
2020-11-03 22:13:27 +0900

    add twinkle functions and more

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function setupTable(rowLength,maximum){
-    let table=""
-    let  column= Math.ceil(maximum/rowLength);
-    console.log(column);
-    for(let i=0;i<column;i++){
-        table+="<div class=\"row\">"
-        for(let k = 1;k<rowLength;k++){
-            let bingoNumber = (i==column-1 && k>5) ? "":i*10+k;
-            table+=`<h1 class=\"col\" id=\"${bingoNumber}\">${bingoNumber}</h1>`;
-        }
-        table+="</div>"
-    }
-    $("#table").append(table);
-}
-
-
-
 function shuffle(array){
     for(let i=array.length-1;i>0;i--){
         let random = Math.floor(Math.random() * (i + 1));
@@ -26,20 +9,67 @@ function shuffle(array){
     return array;
 }
 
-function setRandomizedNumber(array){
-    let number = array[0];
-    console.log(number);
-    $(`#${number}`).addClass("used-number");
-    array.shift();
-    return array;
+async function rollBingo (array){
+        let number = array[0];
+        console.log(number);
+
+        if(!isNaN(number)){
+            let runAroundMax = 75+number;
+
+            const sleep = (ms)=>{
+                return new Promise((f)=>{
+                    setTimeout(f,ms);
+                })
+            };
+
+            for(let i =1;i<=runAroundMax+1;i++){
+                $(`#${i%76}`).addClass("run-around");
+                $(`#${i%75 ==  1 ? maxBingoNum :i%76-1}`).removeClass("run-around");
+                await sleep((Math.pow(i/runAroundMax,3)*400));
+                console.log(i%75);
+            }
+            
+            async function twinkleNumber(number){
+                await sleep(500);
+                for(let i=0;i<3;i++){
+                    $(`#${number}`).removeClass("run-around");
+                    await sleep(500);
+                    $(`#${number}`).addClass("run-around");
+                    await sleep(300);
+                }
+            };
+
+            await twinkleNumber(number);
+
+            $(".table-number").removeClass("run-around");
+            $(`#${number}`).addClass("used-number");
+            array.shift();
+        }
+        return array;
 }
-const maxBingo=75;
-window.onload=()=>{
-    let numbers=[...Array(maxBingo).keys()].map(i=>++i);
+function generateTable(rowLength,maximum){
+    let table="";
+    let  column= Math.ceil(maximum/rowLength);
+
+    for(let i=0;i<column;i++){
+        table+="<div class=\"row\">"
+        for(let k = 1;k<=rowLength;k++){
+            let bingoNumber = (i==column-1 && k>5) ? "" : i*10+k;
+            let numberClass = (i==column-1 && k>5) ? "padding-cell" : "table-number";
+            table+=`<h1 class="col ${numberClass} " id="${bingoNumber}">${bingoNumber}</h1>`;
+        }
+        table+="</div>"
+    }
+    $("#table").append(table);
+}
+const maxBingoNum=75;
+
+window.onload=async()=>{
+    let numbers=[...Array(maxBingoNum).keys()].map(i=>++i);
     shuffle(numbers);
-    $("#start_bingo").click(()=>{
+    $("#start_bingo").click(async()=>{
         console.log("clicked");
-        numbers = setRandomizedNumber(numbers);
+        numbers = await rollBingo(numbers);
     });
-    setupTable(10,maxBingo);
+    generateTable(10,maxBingoNum);
 }
